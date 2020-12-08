0a404eb02503800a1b9b6b5d394ffd5a292fee3e
Your Name <mmuzasantuna@gmail.com>
2020-12-08 20:00:04 +0000

    css file created

--- src/components/SongDetails.js
+++ src/components/SongDetails.js
@@ -1,8 +1,15 @@
+import '../components/'
+
 const SongDetails = ({children, artist, image}) => {
     return (
         <>
         <img src={image} alt="" />
-        <li>{children} by {artist}</li>
+        <li> 
+            <div className = "details">
+                <p>{children}</p>
+                <p>{artist}</p>
+            </div>
+        </li>
         </>
     )
 }
--- src/components/SongList.js
+++ src/components/SongList.js
@@ -5,12 +5,16 @@ const SongList = ({songs}) => {
 
     const songList = songs.map( song => {
         return (
-            <SongDetails key={song.id.attributes['im:id']} image={song['im:image'][1].label} artist={song['im:artist'].label}> {song['im:name'].label}</SongDetails>
+            <SongDetails 
+                key={song.id.attributes['im:id']} 
+                image={song['im:image'][1].label} 
+                artist={song['im:artist'].label}> 
+                {song['im:name'].label}</SongDetails>
         )
     })
      return (
          <>
-         <p>this is the SongList component</p>
+         <p>This is the SongList component</p>
          <ol>
              {songList}
          </ol>
